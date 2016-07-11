f6df5cff3863ebcb37b5bb6d1dd8aaa6ca7efaad
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2016-07-11 13:37:33 +0800

    IAwaiter

---
+++ Meowtrix.ITask/IAwaiter.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Meowtrix.ITask
+{
+    /// <summary>Awaiter interface for awaiting an <see cref="ITask"/>.</summary>
+    /// <remarks>This type is intended for compiler use only.</remarks>
+    public interface IAwaiter : ICriticalNotifyCompletion
+    {
+        /// <summary>Gets a value indicating whether the task being awaited is completed.</summary>
+        /// <remarks>This property is intended for compiler user rather than use directly in code.</remarks>
+        /// <exception cref="System.NullReferenceException">The awaiter was not properly initialized.</exception>
+        bool IsCompleted { get; }
+
+        /// <summary>Ends the await on the completed <see cref="ITask"/>.</summary>
+        /// <exception cref="System.NullReferenceException">The awaiter was not properly initialized.</exception>
+        /// <exception cref="System.Threading.Tasks.TaskCanceledException">The task was canceled.</exception>
+        /// <exception cref="System.Exception">The task completed in a Faulted state.</exception>
+        void GetResult();
+    }
+}
